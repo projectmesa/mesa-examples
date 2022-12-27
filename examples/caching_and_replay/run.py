8ca554a7e7e0ef19c97d113a81d92a117c3a173f
Felix Neubauer <felix@neuby.de>
2022-12-27 11:50:25 +0100

    reformat using black

--- examples/caching_and_replay/run.py
+++ examples/caching_and_replay/run.py
@@ -18,10 +18,12 @@ def get_cache_file_status(_):
     """
     Display an informational text about caching and the status of the cache file (existing versus not existing)
     """
-    return f"This example writes every simulation run into a cache file. Cached runs can be replayed later. " \
-           f"Activate the 'Replay cached run?' toggle to replay the latest cached run. " \
-           f"Only activate the replay when a cache file already exists, otherwise it will fail. Cache file existing: " \
-           f"'{os.path.exists('my_cache_file_path.cache')}'."
+    return (
+        f"This example writes every simulation run into a cache file. Cached runs can be replayed later. "
+        f"Activate the 'Replay cached run?' toggle to replay the latest cached run. "
+        f"Only activate the replay when a cache file already exists, otherwise it will fail. Cache file existing: "
+        f"'{os.path.exists('my_cache_file_path.cache')}'."
+    )
 
 
 server = mesa.visualization.ModularServer(
