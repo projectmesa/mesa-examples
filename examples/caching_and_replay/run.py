6d874774e681597a39ca911116ce619f33c95fb8
Catherine Devlin <devlinch@corning.com>
2023-04-24 18:01:51 -0600

    Remove pathlib changes
    These belong in a separate PR.

--- examples/caching_and_replay/run.py
+++ examples/caching_and_replay/run.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+import os
 
 import mesa
 
@@ -10,21 +10,17 @@ from server import (
 )
 from cacheablemodel import CacheableSchelling
 
-# As 'replay' is a simulation model parameter in this example,
-# we need to make it available as such
+# As 'replay' is a simulation model parameter in this example, we need to make it available as such
 model_params["replay"] = mesa.visualization.Checkbox("Replay cached run?", False)
 
 
 def get_cache_file_status(_):
     """
-    Display an informational text about caching and the status
-    of the cache file (existing versus not existing)
+    Display an informational text about caching and the status of the cache file (existing versus not existing)
     """
-    cache_file = Path("./my_cache_file_path.cache")
     return (
-        f"Only activate the 'Replay cached run?' switch "
-        f"when a cache file already exists, otherwise it will fail. "
-        f"Cache file existing: '{cache_file.exists()}'."
+        f"Only activate the 'Replay cached run?' switch when a cache file already exists, otherwise it will fail. "
+        f"Cache file existing: '{os.path.exists('my_cache_file_path.cache')}'."
     )
 
 
