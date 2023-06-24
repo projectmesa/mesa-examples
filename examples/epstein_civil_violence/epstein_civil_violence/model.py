c9f0eb94f279495ba399b70fa759a47938142e93
Joseph Helbing <helbing@uchicago.edu>
2023-06-23 22:22:28 -0500

    Bug fixes and additions to Epstein Civil Violence
    This update changes a small set of bugs that was causing the model to
    not exhibit the cyclical explosions of active agents seen in the
    original Epstein paper. It also adds some small functionality
    to the browser display, introducing sliders to allow the user
    to change model parameters on the fly, and watch a graph
    of agent state numbers.
    
    Bug Fixes
    - Fix agent state logic
    - Fix Moore vs Von Neuman neighborhood selection to use
        Epstein paper behavior
    - Fix agent vision bug (was 1 and not parameter `vision` when
        calculating neighborhood, which also fixes agent movement issue)
    - Fix arrestee state issue (arrestee was not being reset to "Quiescent",
        and so was counting as "Active" for all nearby agents)
    
    Additions and Changes
    - add model attributes to count agents and display a graph of agent
        states
    - Change display of agents where jailed agents are represented by grey
        square box
    - Added sliders for model attributes

--- examples/epstein_civil_violence/epstein_civil_violence/model.py
+++ examples/epstein_civil_violence/epstein_civil_violence/model.py
@@ -60,10 +60,21 @@ class EpsteinCivilViolence(mesa.Model):
         self.iteration = 0
         self.schedule = mesa.time.RandomActivation(self)
         self.grid = mesa.space.SingleGrid(width, height, torus=True)
+
+        # agent counts
+        self.citizen_count = 0
+        self.cop_count = 0
+        self.jail_count = 0
+        self.active_count = 0
+        self.quiescent_count = 0
+        self.average_jail_term = 0
+
         model_reporters = {
             "Quiescent": lambda m: self.count_type_citizens(m, "Quiescent"),
             "Active": lambda m: self.count_type_citizens(m, "Active"),
             "Jailed": self.count_jailed,
+            "Citizens": self.count_citizens,
+            "Cops": self.count_cops,
         }
         agent_reporters = {
             "x": lambda a: a.pos[0],
@@ -102,6 +113,8 @@ class EpsteinCivilViolence(mesa.Model):
 
         self.running = True
         self.datacollector.collect(self)
+        self.citizen_count = sum(value for value in self.count_agents(self).values())
+        self.cop_count = self.count_cops(self)
 
     def step(self):
         """
@@ -110,6 +123,11 @@ class EpsteinCivilViolence(mesa.Model):
         self.schedule.step()
         # collect data
         self.datacollector.collect(self)
+        # update agent counts
+        self.active_count = self.count_type_citizens(self, "Active")
+        self.quiescent_count = self.count_type_citizens(self, "Quiescent")
+        self.jail_count = self.count_jailed(self)
+        # update iteration
         self.iteration += 1
         if self.iteration > self.max_iters:
             self.running = False
@@ -123,7 +141,7 @@ class EpsteinCivilViolence(mesa.Model):
         for agent in model.schedule.agents:
             if agent.breed == "cop":
                 continue
-            if exclude_jailed and agent.jail_sentence:
+            if exclude_jailed and agent.jail_sentence > 0:
                 continue
             if agent.condition == condition:
                 count += 1
@@ -136,6 +154,40 @@ class EpsteinCivilViolence(mesa.Model):
         """
         count = 0
         for agent in model.schedule.agents:
-            if agent.breed == "citizen" and agent.jail_sentence:
+            if agent.breed == "citizen" and agent.jail_sentence > 0:
                 count += 1
         return count
+
+    @staticmethod
+    def count_citizens(model):
+        """
+        Helper method to count citizens.
+        """
+        count = 0
+        for agent in model.schedule.agents:
+            if agent.breed == "citizen":
+                count += 1
+        return count
+
+    @staticmethod
+    def count_cops(model):
+        """
+        Helper method to count cops.
+        """
+        count = 0
+        for agent in model.schedule.agents:
+            if agent.breed == "cop":
+                count += 1
+        return count
+
+    # combine all agent counts into one method
+    @staticmethod
+    def count_agents(model):
+        """
+        combines the various count methods into one
+        """
+        return {
+            "Quiescent": model.count_type_citizens(model, "Quiescent"),
+            "Active": model.count_type_citizens(model, "Active"),
+            "Jailed": model.count_jailed(model),
+        }
