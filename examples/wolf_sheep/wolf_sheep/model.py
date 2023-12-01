a9b629733d64121f5752b0a71bf0e48246a43f93
Kaitlyn Hutchins <hutch263@msu.edu>
2023-12-01 15:56:35 -0500

    Revert "tried adding energy to wolfsheep class initialization"
    This reverts commit 3b875f3415a8265147eb0eeda6e6be7142535d92.
    
    getting rid of movement's dependance on energy

--- examples/wolf_sheep/wolf_sheep/model.py
+++ examples/wolf_sheep/wolf_sheep/model.py
@@ -43,7 +43,6 @@ class WolfSheep(mesa.Model):
 
     def __init__(
         self,
-        energy,
         width=20,
         height=20,
         initial_sheep=100,
@@ -69,7 +68,7 @@ class WolfSheep(mesa.Model):
                                  once it is eaten
             sheep_gain_from_food: Energy sheep gain from grass, if enabled.
         """
-        super().__init__(energy=None)
+        super().__init__()
         # Set parameters
         self.width = width
         self.height = height
