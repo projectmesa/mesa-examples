1dd169445a625c16d29643d767fed6d20e719559
Kaitlyn Hutchins <hutch263@msu.edu>
2023-12-01 15:57:09 -0500

    Revert "tried to address sheep.step missing the arguement energy"
    This reverts commit 9e350ba95059720b3a84feb027fcdd2c3c146c0e.
    
    getting rid of movement's dependance on energy

--- examples/wolf_sheep/wolf_sheep/model.py
+++ examples/wolf_sheep/wolf_sheep/model.py
@@ -128,8 +128,8 @@ class WolfSheep(mesa.Model):
         self.running = True
         self.datacollector.collect(self)
 
-    def step(self, energy):
-        self.schedule.step(self.energy)
+    def step(self):
+        self.schedule.step()
         # collect data
         self.datacollector.collect(self)
         if self.verbose:
