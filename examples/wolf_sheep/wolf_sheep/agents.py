cf06926153ecf178d4c9cb07bfb03ffadb1a5c2b
Kaitlyn Hutchins <hutch263@msu.edu>
2023-12-01 16:15:34 -0500

    getting rid of energy changes

--- examples/wolf_sheep/wolf_sheep/agents.py
+++ examples/wolf_sheep/wolf_sheep/agents.py
@@ -12,8 +12,9 @@ class Sheep(RandomWalker):
 
     energy = None
 
-    def __init__(self, unique_id, pos, model, moore):
-        super().__init__(unique_id, pos, model, moore=moore, energy=None)
+    def __init__(self, unique_id, pos, model, moore, energy=None):
+        super().__init__(unique_id, pos, model, moore=moore)
+        self.energy = energy
 
     def step(self):
         """
