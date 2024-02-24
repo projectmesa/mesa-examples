4c2d1e9c5af5a30950b2417ce537da564b61ee27
Achal Jain <achaljain232@gmail.com>
2024-02-24 18:15:06 +0530

    Update model.py

--- examples/boid_flockers/boid_flockers/model.py
+++ examples/boid_flockers/boid_flockers/model.py
@@ -8,7 +8,6 @@ Uses numpy arrays to represent vectors.
 import mesa
 import numpy as np
 
-
 class Boid(mesa.Agent):
     """
     A Boid-style flocker agent.
@@ -60,39 +59,40 @@ class Boid(mesa.Agent):
         self.cohere_factor = cohere
         self.separate_factor = separate
         self.match_factor = match
+        self.neighbors = None
 
-    def cohere(self, neighbors):
+    def cohere(self):
         """
         Return the vector toward the center of mass of the local neighbors.
         """
         cohere = np.zeros(2)
-        if neighbors:
-            for neighbor in neighbors:
+        if self.neighbors:
+            for neighbor in self.neighbors:
                 cohere += self.model.space.get_heading(self.pos, neighbor.pos)
-            cohere /= len(neighbors)
+            cohere /= len(self.neighbors)
         return cohere
 
-    def separate(self, neighbors):
+    def separate(self):
         """
         Return a vector away from any neighbors closer than separation dist.
         """
         me = self.pos
-        them = (n.pos for n in neighbors)
+        them = (n.pos for n in self.neighbors)
         separation_vector = np.zeros(2)
         for other in them:
             if self.model.space.get_distance(me, other) < self.separation:
                 separation_vector -= self.model.space.get_heading(me, other)
         return separation_vector
 
-    def match_heading(self, neighbors):
+    def match_heading(self):
         """
         Return a vector of the neighbors' average heading.
         """
         match_vector = np.zeros(2)
-        if neighbors:
-            for neighbor in neighbors:
+        if self.neighbors:
+            for neighbor in self.neighbors:
                 match_vector += neighbor.direction
-            match_vector /= len(neighbors)
+            match_vector /= len(self.neighbors)
         return match_vector
 
     def step(self):
@@ -100,16 +100,15 @@ class Boid(mesa.Agent):
         Get the Boid's neighbors, compute the new vector, and move accordingly.
         """
 
-        neighbors = self.model.space.get_neighbors(self.pos, self.vision, False)
+        self.neighbors = self.model.space.get_neighbors(self.pos, self.vision, False)
         self.direction += (
-            self.cohere(neighbors) * self.cohere_factor
-            + self.separate(neighbors) * self.separate_factor
-            + self.match_heading(neighbors) * self.match_factor
+            self.cohere() * self.cohere_factor
+            + self.separate() * self.separate_factor
+            + self.match_heading() * self.match_factor
         ) / 2
         self.direction /= np.linalg.norm(self.direction)
         new_pos = self.pos + self.direction * self.speed
         self.model.space.move_agent(self, new_pos)
-
 
 class BoidFlockers(mesa.Model):
     """
