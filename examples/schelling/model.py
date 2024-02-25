4426e4e7c5f20d5c74c1cfe779eb2726d2e89470
Achal Jain <achaljain232@gmail.com>
2024-02-25 17:51:03 +0530

    Update model.py

--- examples/schelling/model.py
+++ examples/schelling/model.py
@@ -6,7 +6,7 @@ class SchellingAgent(mesa.Agent):
     Schelling segregation agent
     """
 
-    def __init__(self, pos, model, agent_type, unique_id):
+    def __init__(self, unique_id, model, agent_type):
         """
         Create a new Schelling agent.
 
@@ -16,12 +16,13 @@ class SchellingAgent(mesa.Agent):
            agent_type: Indicator for the agent's type (minority=1, majority=0)
         """
         super().__init__(unique_id, model)
-        self.pos = pos
         self.type = agent_type
 
     def step(self):
         similar = 0
-        for neighbor in self.model.grid.iter_neighbors(self.pos, True):
+        for neighbor in self.model.grid.iter_neighbors(
+            self.pos, moore=True, radius=self.model.radius
+        ):
             if neighbor.type == self.type:
                 similar += 1
 
@@ -37,7 +38,7 @@ class Schelling(mesa.Model):
     Model class for the Schelling segregation model.
     """
 
-    def __init__(self, width=20, height=20, density=0.8, minority_pc=0.2, homophily=3):
+    def __init__(self, height, width, homophily, radius, density, minority_pc=0.5, seed=None):
         """
         Create a new Schelling model.
 
@@ -46,49 +47,47 @@ class Schelling(mesa.Model):
             density: Initial Chance for a cell to populated
             minority_pc: Chances for an agent to be in minority class
             homophily: Minimum number of agents of same class needed to be happy
+            radius: Search radius for checking similarity
+            seed: Seed for Reproducibility
         """
-        super().__init__()
-        self.width = width
+        
+        super().__init__(seed=seed)
         self.height = height
+        self.width = width
         self.density = density
         self.minority_pc = minority_pc
         self.homophily = homophily
+        self.radius = radius
 
         self.schedule = mesa.time.RandomActivation(self)
-        self.grid = mesa.space.SingleGrid(width, height, torus=True)
+        self.grid = mesa.space.SingleGrid(height, width, torus=True)
 
         self.happy = 0
         self.datacollector = mesa.DataCollector(
-            {"happy": "happy"},  # Model-level count of happy agents
-            # For testing purposes, agent's individual x and y
-            {"x": lambda a: a.pos[0], "y": lambda a: a.pos[1]},
+            model_reporters={"happy": "happy"},  # Model-level count of happy agents
         )
 
-        agent_id = 0 # Unique ID for each agent
         # Set up agents
         # We use a grid iterator that returns
         # the coordinates of a cell as well as
         # its contents. (coord_iter)
-        for cell in self.grid.coord_iter():
-            x, y = cell[1]
+        for _cont, pos in self.grid.coord_iter():
             if self.random.random() < self.density:
                 agent_type = 1 if self.random.random() < self.minority_pc else 0
-
-                agent = SchellingAgent(pos=(x, y), model=self, agent_type=agent_type, unique_id=agent_id)
-                self.grid.place_agent(agent, (x, y))
+                agent = SchellingAgent(self.next_id(), self, agent_type)
+                self.grid.place_agent(agent, pos)
                 self.schedule.add(agent)
-                agent_id += 1
 
-        self.running = True
         self.datacollector.collect(self)
 
     def step(self):
         """
-        Run one step of the model. If All agents are happy, halt the model.
+        Run one step of the model.
         """
         self.happy = 0  # Reset counter of happy agents
         self.schedule.step()
-        self.datacollector.collect(self) # collect data
 
+        self.datacollector.collect(self)
+        
         if self.happy == self.schedule.get_agent_count():
             self.running = False
