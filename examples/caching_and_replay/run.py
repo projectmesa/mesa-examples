a8fac326b6e62b71639d2080e6dd374adf532929
Achal Jain <achaljain232@gmail.com>
2024-02-26 17:04:05 +0530

    Update run.py

--- examples/caching_and_replay/run.py
+++ examples/caching_and_replay/run.py
@@ -6,14 +6,14 @@ from server import canvas_element, get_happy_agents, happy_chart, model_params
 
 # As 'replay' is a simulation model parameter in this example, we need to make it available as such
 model_params["replay"] = mesa.visualization.Checkbox("Replay cached run?", False)
-model_params["cache_file_path"] = "my_cache_file_path.cache"
+model_params["cache_file_path"] = "./my_cache_file_path.cache"
 
 
 def get_cache_file_status(_):
     """
     Display an informational text about caching and the status of the cache file (existing versus not existing)
     """
-    cache_file = Path("./" + model_params["cache_file_path"])
+    cache_file = Path(model_params["cache_file_path"])
     return (
         f"Only activate the 'Replay cached run?' switch when a cache file already exists, otherwise it will fail. "
         f"Cache file existing: '{cache_file.exists()}'."
