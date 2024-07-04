7731c8069946d762759d0a677391931a2f4fc169
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-07-04 16:15:46 +0200

    Fix pre-commit
    Make pre-commit fully green again

--- gis/agents_and_networks/src/logger.py
+++ gis/agents_and_networks/src/logger.py
@@ -7,9 +7,9 @@ def logger(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         logger = logging.getLogger(func.__name__)
-        logger.info("About to run %s" % func.__name__)
+        logger.info(f"About to run {func.__name__}")
         out = func(*args, **kwargs)
-        logger.info("Done running %s" % func.__name__)
+        logger.info(f"Done running {func.__name__}")
         return out
 
     return wrapper
