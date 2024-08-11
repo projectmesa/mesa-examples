8fffea152a9b2354df3c13d5a3aac60b0f426906
jayash <139613263+jayash1973@users.noreply.github.com>
2024-08-11 23:51:20 +0530

    Update test_wolf_sheep.py

--- examples/wolf_sheep_experimental/test_wolf_sheep.py
+++ examples/wolf_sheep_experimental/test_wolf_sheep.py
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 from wolf_sheep.wolf_sheep import WolfSheep
 
 def test_wolf_sheep():
     model = WolfSheep(seed=15)
     assert model is not None
-=======
-from wolf_sheep.wolf_sheep import WolfSheep
-
-
-def test_wolf_sheep():
-    model = WolfSheep(seed=15)
-    assert model is not None
->>>>>>> 032388f056dcf0b98004478f2df1939a228a8863
