77b5e193fae76872411f893e9e43d3e92c2e4793
Zak Jost <zak.jost@airbnb.com>
2024-08-12 13:19:45 -0700

    simple fix, isort, clean

--- examples/aco_tsp/run_tsp.py
+++ examples/aco_tsp/run_tsp.py
@@ -1,15 +1,15 @@
 from collections import defaultdict
 
 import matplotlib.pyplot as plt
+from aco_tsp.model import AcoTspModel, TSPGraph
 
-from aco_tsp.model import AcoTspModel, TSPGraph # create_graph
 
-    
 def main():
     # tsp_graph = TSPGraph.from_random(num_cities=20, seed=1)
     tsp_graph = TSPGraph.from_tsp_file("aco_tsp/data/kroA100.tsp")
     model_params = {
-        "num_agents": tsp_graph.num_cities, "tsp_graph": tsp_graph,
+        "num_agents": tsp_graph.num_cities,
+        "tsp_graph": tsp_graph,
     }
     number_of_episodes = 50
 
@@ -25,7 +25,9 @@ def main():
         model.step()
         results["best_distance"].append(model.best_distance)
         results["best_path"].append(model.best_path)
-        print(f"Episode={e+1}; Min. distance={model.best_distance}; pheromone_1_8={model.grid.G[17][15]['pheromone']}")
+        print(
+            f"Episode={e+1}; Min. distance={model.best_distance}; pheromone_1_8={model.grid.G[17][15]['pheromone']}"
+        )
         if model.best_distance < best_distance:
             best_distance = model.best_distance
             best_path = model.best_path
@@ -35,7 +37,7 @@ def main():
     print(f"Best path: {best_path}")
     print(model.datacollector.get_model_vars_dataframe())
 
-    fig, ax = plt.subplots()
+    _, ax = plt.subplots()
     ax.plot(results["best_distance"])
     ax.set(xlabel="Episode", ylabel="Best distance", title="Best distance per episode")
     plt.show()
