04ad95d0e541d674e6334aaadd5428adbc3071a4
jayash <139613263+jayash1973@users.noreply.github.com>
2024-08-12 18:11:52 +0530

    Update model.py

--- examples/wolf_sheep_experimental/model.py
+++ examples/wolf_sheep_experimental/model.py
@@ -9,36 +9,19 @@ Replication of the model found in NetLogo:
     Northwestern University, Evanston, IL.
 """
 
+import sys
 import mesa
-from .agents import GrassPatch, Sheep, Wolf
-from .scheduler import RandomActivationByTypeFiltered
+from mesa import Model
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
 
+sys.path.append('mesa-examples\\examples\\epstein_civil_violence_experimental')
 
-class WolfSheep(mesa.Model):
-    """
-    Wolf-Sheep Predation Model
-    """
+from agents import GrassPatch, Sheep, Wolf
 
-    height = 20
-    width = 20
-
-    initial_sheep = 100
-    initial_wolves = 50
-
-    sheep_reproduce = 0.04
-    wolf_reproduce = 0.05
-
-    wolf_gain_from_food = 20
-
-    grass = False
-    grass_regrowth_time = 30
-    sheep_gain_from_food = 4
-
-    verbose = False  # Print-monitoring
-
-    description = (
-        "A model for simulating wolf and sheep (predator-prey) ecosystem modelling."
-    )
+class WolfSheep(Model):
+    """ Wolf-Sheep Predation Model """
 
     def __init__(
         self,
@@ -55,7 +38,6 @@ class WolfSheep(mesa.Model):
     ):
         """
         Create a new Wolf-Sheep model with the given parameters.
-
         Args:
             initial_sheep: Number of sheep to start with
             initial_wolves: Number of wolves to start with
@@ -63,12 +45,10 @@ class WolfSheep(mesa.Model):
             wolf_reproduce: Probability of each wolf reproducing each step
             wolf_gain_from_food: Energy a wolf gains from eating a sheep
             grass: Whether to have the sheep eat grass for energy
-            grass_regrowth_time: How long it takes for a grass patch to regrow
-                                 once it is eaten
+            grass_regrowth_time: How long it takes for a grass patch to regrow once it is eaten
             sheep_gain_from_food: Energy sheep gain from grass, if enabled.
         """
         super().__init__()
-        # Set parameters
         self.width = width
         self.height = height
         self.initial_sheep = initial_sheep
@@ -80,15 +60,20 @@ class WolfSheep(mesa.Model):
         self.grass_regrowth_time = grass_regrowth_time
         self.sheep_gain_from_food = sheep_gain_from_food
 
-        self.schedule = RandomActivationByTypeFiltered(self)
-        self.grid = mesa.space.MultiGrid(self.width, self.height, torus=True)
+        self.schedule = RandomActivation(self)
+        self.grid = MultiGrid(self.width, self.height, torus=True)
 
-        # Create sheep:
+        self.datacollector = DataCollector(
+            model_reporters={"Wolf/Sheep Ratio": get_wolf_sheep_ratio},
+            agent_reporters={"Energy": "energy"}
+        )
+
+        # Create sheep
         for i in range(self.initial_sheep):
             x = self.random.randrange(self.width)
             y = self.random.randrange(self.height)
             energy = self.random.randrange(2 * self.sheep_gain_from_food)
-            sheep = Sheep(self.next_id(), self, True, energy)
+            sheep = Sheep(self.next_id(), self, True, energy, self.sheep_reproduce, self.sheep_gain_from_food)
             self.grid.place_agent(sheep, (x, y))
             self.schedule.add(sheep)
 
@@ -97,7 +82,7 @@ class WolfSheep(mesa.Model):
             x = self.random.randrange(self.width)
             y = self.random.randrange(self.height)
             energy = self.random.randrange(2 * self.wolf_gain_from_food)
-            wolf = Wolf(self.next_id(), self, True, energy)
+            wolf = Wolf(self.next_id(), self, True, energy, self.wolf_reproduce, self.wolf_gain_from_food)
             self.grid.place_agent(wolf, (x, y))
             self.schedule.add(wolf)
 
@@ -105,12 +90,10 @@ class WolfSheep(mesa.Model):
         if self.grass:
             for agent, (x, y) in self.grid.coord_iter():
                 fully_grown = self.random.choice([True, False])
-
                 if fully_grown:
                     countdown = self.grass_regrowth_time
                 else:
                     countdown = self.random.randrange(self.grass_regrowth_time)
-
                 patch = GrassPatch(self.next_id(), self, fully_grown, countdown)
                 self.grid.place_agent(patch, (x, y))
                 self.schedule.add(patch)
@@ -125,3 +108,9 @@ class WolfSheep(mesa.Model):
     def run_model(self, step_count=200):
         for _ in range(step_count):
             self.step()
+
+def get_wolf_sheep_ratio(model):
+    wolf_count = sum(isinstance(agent, Wolf) for agent in model.schedule.agents)
+    sheep_count = sum(isinstance(agent, Sheep) for agent in model.schedule.agents)
+    ratio = wolf_count / sheep_count if sheep_count > 0 else float("inf")
+    return ratio
