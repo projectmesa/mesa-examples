541b0046501577a0bd96946d7df61966d5cde48a
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-08-29 15:38:36 +0200

    GoL_fast: Add minimal Solara visualisation
    Add minimal Solara visualisation, to control model parameters.

---
+++ examples/conways_game_of_life_fast/app.py
@@ -0,0 +1,29 @@
+from mesa.visualization import SolaraViz
+from model import GameOfLifeModel
+
+model_params = {
+    "width": {
+        "type": "SliderInt",
+        "value": 10,
+        "label": "Width",
+        "min": 5,
+        "max": 25,
+        "step": 1,
+    },
+    "height": {
+        "type": "SliderInt",
+        "value": 10,
+        "label": "Height",
+        "min": 5,
+        "max": 25,
+        "step": 1,
+    },
+}
+
+page = SolaraViz(
+    GameOfLifeModel,
+    model_params,
+    space_drawer=None,
+    name="Game of Life Model",
+)
+page  # noqa
