a304bfd39177ea1343a64794cdde892453b08037
Wang Boyu <boyu.wby@gmail.com>
2024-09-03 10:04:52 -0400

    gis: fix user warning when testing geo schelling example
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    update

--- gis/geo_schelling/server.py
+++ gis/geo_schelling/server.py
@@ -38,9 +38,7 @@ def schelling_draw(agent):
 
 
 happy_element = HappyElement()
-map_element = mg.visualization.MapModule(
-    schelling_draw, [52, 12], 4, tiles=xyz.CartoDB.Positron
-)
+map_element = mg.visualization.MapModule(schelling_draw, tiles=xyz.CartoDB.Positron)
 happy_chart = mesa.visualization.ChartModule([{"Label": "happy", "Color": "Black"}])
 server = mesa.visualization.ModularServer(
     GeoSchelling, [map_element, happy_element, happy_chart], "Schelling", model_params
