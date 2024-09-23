1a017802d92573e80bf631cce7e4e066fdf3106c
Vítor Fróis <vitor0frois@gmail.com>
2024-09-23 20:30:10 -0300

    update var name

--- examples/ising/ising/spin.py
+++ examples/ising/ising/spin.py
@@ -7,12 +7,12 @@ class Spin(mesa.Agent):
     UP = 1
     DOWN = -1
 
-    def __init__(self, pos, model, init_state):
+    def __init__(self, position, model, init_state):
         """
         Create a cell, in the given state, at the given x, y position.
         """
-        super().__init__(pos, model)
-        self.x, self.y = pos
+        super().__init__(position, model)
+        self.x, self.y = position
         self.state = init_state
 
     def neighbors(self):
