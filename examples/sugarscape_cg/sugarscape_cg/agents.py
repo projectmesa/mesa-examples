bbec7d767e65ae2084b6e858c76d433524fcda72
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-09-24 12:11:35 +0200

    Address duplicate position warning (#122)

--- examples/sugarscape_cg/sugarscape_cg/agents.py
+++ examples/sugarscape_cg/sugarscape_cg/agents.py
@@ -17,11 +17,8 @@ def get_distance(pos_1, pos_2):
 
 
 class SsAgent(mesa.Agent):
-    def __init__(
-        self, unique_id, pos, model, moore=False, sugar=0, metabolism=0, vision=0
-    ):
+    def __init__(self, unique_id, model, moore=False, sugar=0, metabolism=0, vision=0):
         super().__init__(unique_id, model)
-        self.pos = pos
         self.moore = moore
         self.sugar = sugar
         self.metabolism = metabolism
@@ -74,7 +71,7 @@ class SsAgent(mesa.Agent):
 
 
 class Sugar(mesa.Agent):
-    def __init__(self, unique_id, pos, model, max_sugar):
+    def __init__(self, unique_id, model, max_sugar):
         super().__init__(unique_id, model)
         self.amount = max_sugar
         self.max_sugar = max_sugar
