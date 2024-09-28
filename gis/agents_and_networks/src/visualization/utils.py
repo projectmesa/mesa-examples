98978843371592fc82b478f0bc63e17268fe3125
Wang Boyu <boyu.wby@gmail.com>
2024-09-28 10:54:54 -0400

    gis: update four examples to use solara viz

--- gis/agents_and_networks/src/visualization/utils.py
+++ gis/agents_and_networks/src/visualization/utils.py
@@ -3,6 +3,48 @@ import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import solara
+
+from ..agent.building import Building
+from ..agent.commuter import Commuter
+from ..agent.geo_agents import Driveway, LakeAndRiver, Walkway
+
+
+def make_plot_clock(model):
+    return solara.Markdown(f"**Day {model.day}, {model.hour:02d}:{model.minute:02d}**")
+
+
+def agent_draw(agent):
+    portrayal = {}
+    portrayal["color"] = "White"
+    if isinstance(agent, Driveway):
+        portrayal["color"] = "#D08004"
+    elif isinstance(agent, Walkway):
+        portrayal["color"] = "Brown"
+    elif isinstance(agent, LakeAndRiver):
+        portrayal["color"] = "#04D0CD"
+    elif isinstance(agent, Building):
+        portrayal["color"] = "Grey"
+        # if agent.function is None:
+        #     portrayal["color"] = "Grey"
+        # elif agent.function == 1.0:
+        #     portrayal["color"] = "Blue"
+        # elif agent.function == 2.0:
+        #     portrayal["color"] = "Green"
+        # else:
+        #     portrayal["color"] = "Grey"
+    elif isinstance(agent, Commuter):
+        if agent.status == "home":
+            portrayal["color"] = "Green"
+        elif agent.status == "work":
+            portrayal["color"] = "Blue"
+        elif agent.status == "transport":
+            portrayal["color"] = "Red"
+        else:
+            portrayal["color"] = "Grey"
+        portrayal["radius"] = "5"
+        portrayal["fillOpacity"] = 1
+    return portrayal
 
 
 def plot_commuter_status_count(model_vars_df: pd.DataFrame) -> None:
