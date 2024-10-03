a8b71b2323ace0f654c05f7ab051a9d7ef1f24fc
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-10-03 15:01:13 +0200

    GoL_fast: Visualize the Game of Life cells using the PropertyLayer

--- examples/conways_game_of_life_fast/app.py
+++ examples/conways_game_of_life_fast/app.py
@@ -1,35 +1,41 @@
-from mesa.visualization import SolaraViz, make_plot_measure
+from mesa.visualization import SolaraViz, make_plot_measure, make_space_matplotlib
 from model import GameOfLifeModel
+
+propertylayer_portrayal = {
+    "cell_layer": {
+        "color": "Black",
+        "alpha": 1,
+    },
+}
 
 model_params = {
     "width": {
         "type": "SliderInt",
-        "value": 10,
+        "value": 20,
         "label": "Width",
         "min": 5,
-        "max": 25,
+        "max": 40,
         "step": 1,
     },
     "height": {
         "type": "SliderInt",
-        "value": 10,
+        "value": 20,
         "label": "Height",
         "min": 5,
-        "max": 25,
+        "max": 40,
         "step": 1,
     },
 }
 
-gol = GameOfLifeModel(10, 10)
+gol = GameOfLifeModel()
 
+layer_viz = make_space_matplotlib(propertylayer_portrayal=propertylayer_portrayal)
 TotalAlivePlot = make_plot_measure("Cells alive")
 FractionAlivePlot = make_plot_measure("Fraction alive")
 
-
 page = SolaraViz(
     gol,
-    components=[TotalAlivePlot, FractionAlivePlot],
+    components=[layer_viz, TotalAlivePlot, FractionAlivePlot],
     model_params=model_params,
     name="Game of Life Model",
 )
-page  # noqa
