28d80c8c8a78989013490c1a79e97f6ce5a12e1d
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2024-10-07 13:44:51 +0200

    2 more examples moved

--- examples/hex_snowflake/hex_snowflake/cell.py
+++ examples/hex_snowflake/hex_snowflake/cell.py
@@ -1,39 +1,37 @@
 import mesa
 
+from mesa.experimental.cell_space import FixedAgent
 
-class Cell(mesa.Agent):
+
+class Cell(FixedAgent):
     """Represents a single ALIVE or DEAD cell in the simulation."""
 
     DEAD = 0
     ALIVE = 1
 
-    def __init__(self, pos, model, init_state=DEAD):
+    def __init__(self, cell, model, init_state=DEAD):
         """
         Create a cell, in the given state, at the given x, y position.
         """
         super().__init__(model)
-        self.x, self.y = pos
+        self.cell = cell
         self.state = init_state
-        self._nextState = None
-        self.isConsidered = False
+        self._next_state = None
+        self.is_considered = False
 
     @property
     def is_alive(self):
         return self.state == self.ALIVE
 
     @property
-    def neighbors(self):
-        return self.model.grid.iter_neighbors((self.x, self.y))
-
-    @property
     def considered(self):
-        return self.isConsidered is True
+        return self.is_considered is True
 
     def determine_state(self):
         """
         Compute if the cell will be dead or alive at the next tick. A dead
         cell will become alive if it has only one neighbor. The state is not
-        changed here, but is just computed and stored in self._nextState,
+        changed here, but is just computed and stored in self._next_state,
         because our current state may still be necessary for our neighbors
         to calculate their next state.
         When a cell is made alive, its neighbors are able to be considered
@@ -41,20 +39,20 @@ class Cell(mesa.Agent):
         for performance reasons.
         """
         # assume no state change
-        self._nextState = self.state
+        self._next_state = self.state
 
-        if not self.is_alive and self.isConsidered:
+        if not self.is_alive and self.is_considered:
             # Get the neighbors and apply the rules on whether to be alive or dead
             # at the next tick.
-            live_neighbors = sum(neighbor.is_alive for neighbor in self.neighbors)
+            live_neighbors = sum(neighbor.is_alive for neighbor in self.cell.neighborhood.agents)
 
             if live_neighbors == 1:
-                self._nextState = self.ALIVE
-                for a in self.neighbors:
-                    a.isConsidered = True
+                self._next_state = self.ALIVE
+                for a in self.cell.neighborhood.agents:
+                    a.is_considered = True
 
     def assume_state(self):
         """
         Set the state to the new computed state
         """
-        self.state = self._nextState
+        self.state = self._next_state
