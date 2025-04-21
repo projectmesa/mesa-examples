fd77f43b5aa1e3668966475b00556329ea70a74f
Tom Pike <tpike3@gmu.edu>
2025-04-21 05:32:46 -0400

    fix test

--- examples/warehouse/model.py
+++ examples/warehouse/model.py
@@ -1,14 +1,15 @@
 import mesa
-from agents import (
+from mesa.discrete_space import OrthogonalMooreGrid
+from mesa.discrete_space.cell_agent import CellAgent
+from mesa.experimental.meta_agents.meta_agent import create_meta_agent
+
+from examples.warehouse.agents import (
     InventoryAgent,
     RouteAgent,
     SensorAgent,
     WorkerAgent,
 )
-from make_warehouse import make_warehouse
-from mesa.discrete_space import OrthogonalMooreGrid
-from mesa.discrete_space.cell_agent import CellAgent
-from mesa.experimental.meta_agents.meta_agent import create_meta_agent
+from examples.warehouse.make_warehouse import make_warehouse
 
 # Constants for configuration
 LOADING_DOCKS = [(0, 0, 0), (0, 2, 0), (0, 4, 0), (0, 6, 0), (0, 8, 0)]
